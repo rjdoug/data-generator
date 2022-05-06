5747853cf758bdee4792a3177f4783b207507735
Riley <rileydouglas@outlook.com>
2022-05-06 17:58:54 +1200

    medication done

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/BlaviButcher/data-generator
 
 go 1.18
+
+require github.com/tjarratt/babble v0.0.0-20210505082055-cbca2a4833c1 // indirect
--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+
+	"github.com/tjarratt/babble"
 )
 
 func main() {
-	generateMedication(1)
+	generateMedication(10)
 }
 
 // id NUMBER GENERATED ALWAYS AS IDENTITY,
@@ -21,16 +25,31 @@ func generateMedication(dataLength int) []string {
 	var scientificName, brand string
 	var cost float32
 
-	for i := 0; i < dataLength; i++ {
+	var lines []string
+
+	babbler := babble.NewBabbler()
+	babbler.Count = 1
+
+	for i := 1; i <= dataLength; i++ {
 		id = i
-		isSubsidised = rand.Intn(1)
-		scientificName = "word"
-		brand = "test"
+		isSubsidised = rand.Intn(2)
+		scientificName = babbler.Babble()
+		brand = babbler.Babble()
 		cost = 3.45
+
+		line := fmt.Sprintf("INSER INTO medication (id, scientific_name, cost, isSubsidised, brand) VALUES (%d, %d, %s, %s, %.2f)\n",
+			id, isSubsidised, scientificName, brand, cost)
+
+		lines = append(lines, line)
 	}
 
-	line := fmt.Sprintf(`INSER INTO medication (id, scientific_name, cost, isSubsidised, brand) VALUES %d, %d, %s, %s, %.2f\n`,
-		id, isSubsidised, scientificName, brand, cost)
-	fmt.Println(line)
+	fmt.Print(lines)
 	return nil
+}
+
+func writeFile(file string, lines []string) error {
+	f, err := os.Create(file)
+	if err != nil {
+		return fmt.Errorf("")
+	}
 }
